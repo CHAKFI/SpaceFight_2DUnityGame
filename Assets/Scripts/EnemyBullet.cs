1bbc2418746a9ad2b9c3a1e28c7da282fdcde537
CHAKFI Ahmed <ahmedchakfi1999@gmail.com>
2022-12-02 22:19:02 +0000

    Destroy bullets if goes outside the screen limites

--- Assets/Scripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyBullet.cs
@@ -25,12 +25,35 @@ public class EnemyBullet : MonoBehaviour
     public void SetDirection(Vector2 direction)
     {
         _direction = direction.normalized; // normaliser la direction pour avoir un vecteur "unit"
-        isReady = true;
+        isReady = true; 
     }
     
     // Update is called once per frame
     void Update()
     {
-        
+        if (isReady)
+        {
+            // obtenir la position actuel des bulles
+            Vector2 position = transform.position; 
+            
+            // compter la nouvelle position des bulles
+            position += _direction * speed * Time.deltaTime;
+            
+            // mettre à jour la position des bulles
+            transform.position = position;
+
+            
+            Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0,0)); // C'est la limite de l'écran en bas à gauche
+            Vector2 max = Camera.main.ViewportToScreenPoint(new Vector2(1,1)); // C'est la limite de l'écran en haut à droite
+            
+            // Il faut retirer et faire disparaître les bulles qui dépasse la limite de l'ecran
+
+            if ((transform.position.x < min.x) || (transform.position.x > max.x) || 
+                (transform.position.y < min.y) || (transform.position.y > max.y))
+            {
+                Destroy(gameObject);
+            }
+            
+        }
     }
 }
